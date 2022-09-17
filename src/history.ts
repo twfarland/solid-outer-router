f9be0b0ba49ac8d2938ca172561cfb605829ab01
Tim Farland <twfarland@gmail.com>
2022-09-18 09:29:41 +1200

    initial

--- src/history.ts
+++ src/history.ts
@@ -1,37 +1,29 @@
-import { updateLocation } from './signals';
+import { setLocation } from "./signals";
 
 // non destructively tap into the history api to update signals
 
 export function initialize(history: History) {
   const { pushState, replaceState } = history;
 
-  history.pushState = (
-    data: any,
-    unused: string,
-    url?: string | URL | null
-  ) => {
-    const ret = pushState.call(history, data, unused, url);
-    updateLocation();
+  history.pushState = (data: unknown, unused: string, url?: string | URL | null) => {
+    const ret = pushState.call(history, data, unused, url?.toString());
+    setLocation(window.location);
     return ret;
   };
 
-  history.replaceState = (
-    data: any,
-    unused: string,
-    url?: string | URL | null
-  ) => {
-    const ret = replaceState.call(history, data, unused, url);
-    updateLocation();
+  history.replaceState = (data: unknown, unused: string, url?: string | URL | null) => {
+    const ret = replaceState.call(history, data, unused, url?.toString());
+    setLocation(window.location);
     return ret;
   };
 
-  window.addEventListener('popstate', updateLocation);
+  window.addEventListener("popstate", () => setLocation(window.location));
 }
 
 export function navigate(url: string | URL, replace?: boolean) {
   if (replace) {
-    history.replaceState({}, '', url);
+    history.replaceState({}, "", url.toString());
   } else {
-    history.pushState({}, '', url);
+    history.pushState({}, "", url.toString());
   }
 }
--- src/signals.ts
+++ src/signals.ts
@@ -1,14 +1,14 @@
-import { createSignal } from 'solid-js';
-import { LocationState, MatchedRoute, Route, RouteParams } from './types';
+import { createSignal } from "solid-js";
+import { LocationState, MatchedRoute, Route, RouteParams } from "./types";
 
 // source signals
 const [_location, _setLocation] = createSignal<LocationState>({ location });
-const [_routes, _setRoutes] = createSignal<Route[]>([]);
-export const setRoutes = _setRoutes;
-
-export const updateLocation = () => {
+export const setLocation = (location: Location) => {
   _setLocation({ location });
 };
+
+const [_routes, _setRoutes] = createSignal<Route[]>([]);
+export const setRoutes = _setRoutes;
 
 // derived signals
 const matched = () => matchRoute(_routes(), _location());
@@ -16,15 +16,12 @@ export const params = () => matched().params;
 export const route = () => matched().route[1]();
 
 // routing pure functions for matching
-function matchRoute(
-  routes: Route[],
-  { location }: LocationState
-): MatchedRoute {
-  const parts = location.pathname.split('/').filter(s => s);
+function matchRoute(routes: Route[], { location }: LocationState): MatchedRoute {
+  const parts = location.pathname.split("/").filter(s => s);
 
   for (const route of routes) {
     const params = bindRoute(
-      route[0].split('/').filter(s => s),
+      route[0].split("/").filter(s => s),
       parts
     );
 
@@ -33,7 +30,7 @@ function matchRoute(
     }
   }
 
-  return { route: ['', () => null], params: {} };
+  return { route: ["", () => null], params: {} };
 }
 
 function bindRoute(route: string[], path: string[]) {
@@ -46,7 +43,7 @@ function bindRoute(route: string[], path: string[]) {
   for (let i = 0; i < route.length; i++) {
     const routePart = route[i];
     const pathPart = path[i];
-    if (routePart[0] === ':') {
+    if (routePart[0] === ":") {
       params[routePart.substring(1)] = pathPart;
     } else if (routePart !== pathPart) {
       return undefined;
