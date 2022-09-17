0ab4721e31471441360d214d913c82b2844112bc
Tim Farland <twfarland@gmail.com>
2022-09-17 15:26:36 +1200

    wrap history initializer

--- src/history.ts
+++ src/history.ts
@@ -2,25 +2,31 @@ import { updateLocation } from './signals';
 
 // non destructively tap into the history api to update signals
 
-const { pushState, replaceState } = history;
+export function initialize(history: History) {
+  const { pushState, replaceState } = history;
 
-history.pushState = (data: any, unused: string, url?: string | URL | null) => {
-  const ret = pushState.call(history, data, unused, url);
-  updateLocation();
-  return ret;
-};
+  history.pushState = (
+    data: any,
+    unused: string,
+    url?: string | URL | null
+  ) => {
+    const ret = pushState.call(history, data, unused, url);
+    updateLocation();
+    return ret;
+  };
 
-history.replaceState = (
-  data: any,
-  unused: string,
-  url?: string | URL | null
-) => {
-  const ret = replaceState.call(history, data, unused, url);
-  updateLocation();
-  return ret;
-};
+  history.replaceState = (
+    data: any,
+    unused: string,
+    url?: string | URL | null
+  ) => {
+    const ret = replaceState.call(history, data, unused, url);
+    updateLocation();
+    return ret;
+  };
 
-window.addEventListener('popstate', updateLocation);
+  window.addEventListener('popstate', updateLocation);
+}
 
 export function navigate(url: string | URL, replace?: boolean) {
   if (replace) {
